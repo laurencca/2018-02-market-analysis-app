4655a7643a64a7b5d44362fab1408542157f5f9c
laurencca <lcarrollallan@gmail.com>
2018-03-05 20:43:09 -0800

    add vote tracker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,32 @@
 'use strict';
 
-var images = ['./images/bag.jpg', './images/banana.jpg', './images/boots.jpg', './images/chair.jpg', './images/cthulhu.jpg', './images/dragon.jpg', './images/pen.jpg', './images/scissors.jpg', './images/shark.jpg', './images/sweep.jpg', './images/unicorn.jpg', './images/usb.jpg', './images/water_can.jpg', './images/wine_glass.jpg'];
+var imageName = '';
+var imageLocation = '';
 var carousel = document.getElementById('images-holder');
+
+//constructor function for images
+var Image = function(imageName, imageLocation) {
+  this.imageName = imageName;
+  this.imageLocation = imageLocation;
+  this.vote = 0;
+}
+
+//images array
+var images = [];
+images.push(new Image('bag', 'bag.jpg'))
+images.push(new Image('banana', 'banana.jpg'))
+images.push(new Image('boots', 'boots.jpg'))
+images.push(new Image('chair', 'chair.jpg'))
+images.push(new Image('cthulhu', 'cthulhu.jpg'))
+images.push(new Image('dragon', 'dragon.jpg'))
+images.push(new Image('pen', 'pen.jpg'))
+images.push(new Image('scissors', 'scissors.jpg'))
+images.push(new Image('shark', 'shark.jpg'))
+images.push(new Image('sweep', 'sweep.jpg'))
+images.push(new Image('unicorn', 'unicorn.jpg'))
+images.push(new Image('usb', 'usb.jpg'))
+images.push(new Image('water_can', 'water_can.jpg'))
+images.push(new Image('wine_glass', 'wine_glass.jpg'))
 
 //function to get one random image from the images array
 function getRandomImage() {
@@ -25,16 +50,41 @@ function loadImage() {
 
 //function to display results from loadImage function on page
 function buildCarousel() {
+  var holder = document.getElementById('images-holder');
+  holder.innerHTML = '';
   var threeImages = loadImage();
   var image = document.createElement('img');
-  image.src = threeImages[0];
-  document.getElementById('images-holder').appendChild(image);
+  image.src = 'images/' + threeImages[0].imageLocation;
+  holder.appendChild(image);
   image = document.createElement('img');
-  image.src = threeImages[1];
-  document.getElementById('images-holder').appendChild(image);
+  image.src = 'images/' + threeImages[1].imageLocation;
+  holder.appendChild(image);
   image = document.createElement('img');
-  image.src = threeImages[2];
-  document.getElementById('images-holder').appendChild(image);
+  image.src = 'images/' + threeImages[2].imageLocation;
+  holder.appendChild(image);
 }
 
+//function to record image clicks
+function addListener() {
+  document.getElementById('images-holder').addEventListener('click', registerImageClick);
+}
+
+function registerImageClick(event) {
+  if (event.target.tagName == 'IMG') {
+    var index = event.target.src.lastIndexOf("/");
+    var imageLocation = event.target.src.substring(index + 1);
+    console.log(imageLocation);
+    for (var index = 0; index < images.length; index++) {
+      if (images[index].imageLocation.indexOf(imageLocation) != -1) {
+        images[index].vote += 1;
+        console.log(images[index].vote);
+      }
+    }
+    buildCarousel();
+  }
+}
+
+
+
 window.addEventListener('load', buildCarousel);
+window.addEventListener('load', addListener);
