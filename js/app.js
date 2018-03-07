e92b7f67619235ad3b705d90b778269b572796e8
laurencca <lcarrollallan@gmail.com>
2018-03-06 19:11:29 -0800

    updated code from class10

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
 'use strict';
 
-var images = ['./images/bag.jpg', './images/banana.jpg', './images/boots.jpg', './images/chair.jpg', './images/cthulhu.jpg', './images/dragon.jpg', './images/pen.jpg', './images/scissors.jpg', './images/shark.jpg', './images/sweep.jpg', './images/unicorn.jpg', './images/usb.jpg', './images/water_can.jpg', './images/wine_glass.jpg'];
+var imageName = '';
+var imageLocation = '';
 var carousel = document.getElementById('images-holder');
+var voteCounter = 0;
+
+//constructor function for images
+var Image = function(imageName, imageLocation) {
+  this.imageName = imageName;
+  this.imageLocation = imageLocation;
+  this.vote = 0;
+}
+
+//images array
+var images = [];
+images.push(new Image('Bag', 'bag.jpg'))
+images.push(new Image('Banana', 'banana.jpg'))
+images.push(new Image('Boots', 'boots.jpg'))
+images.push(new Image('Chair', 'chair.jpg'))
+images.push(new Image('Cthulhu', 'cthulhu.jpg'))
+images.push(new Image('Dragon', 'dragon.jpg'))
+images.push(new Image('Pen', 'pen.jpg'))
+images.push(new Image('Scissors', 'scissors.jpg'))
+images.push(new Image('Shark', 'shark.jpg'))
+images.push(new Image('Sweep', 'sweep.jpg'))
+images.push(new Image('Unicorn', 'unicorn.jpg'))
+images.push(new Image('Usb', 'usb.jpg'))
+images.push(new Image('Water Can', 'water_can.jpg'))
+images.push(new Image('Wine Glass', 'wine_glass.jpg'))
 
 //function to get one random image from the images array
 function getRandomImage() {
@@ -20,34 +46,65 @@ function loadImage() {
       threeImages.push(randomImage);
     }
   }
-  console.log(threeImages);
+  return threeImages;
 }
 
 //function to display results from loadImage function on page
 function buildCarousel() {
-  var image = loadImage();
-    document.getElementById('images-holder').appendChild(image);
+  var holder = document.getElementById('images-holder');
+  holder.innerHTML = '';
+  var threeImages = loadImage();
+  var image = document.createElement('img');
+  image.src = 'images/' + threeImages[0].imageLocation;
+  holder.appendChild(image);
+  image = document.createElement('img');
+  image.src = 'images/' + threeImages[1].imageLocation;
+  holder.appendChild(image);
+  image = document.createElement('img');
+  image.src = 'images/' + threeImages[2].imageLocation;
+  holder.appendChild(image);
 }
 
-buildCarousel();
+//function to record image clicks
+function addListener() {
+  document.getElementById('images-holder').addEventListener('click', registerImageClick);
+}
 
-window.addEventListener('load', loadImage);
+//function to track which images are clicked and stop voting after 15 rounds
+function registerImageClick(event) {
+  if (event.target.tagName == 'IMG') {
+    var index = event.target.src.lastIndexOf("/");
+    var imageLocation = event.target.src.substring(index + 1);
+    for (var index = 0; index < images.length; index++) {
+      if (images[index].imageLocation.indexOf(imageLocation) != -1) {
+        images[index].vote += 1;
+        voteCounter += 1;
+      }
+    }
+    if (voteCounter == 15) {
+      alert('Voting Complete! Click "Show Results" button below.');
+    } else {
+      buildCarousel();
+    }
+  }
+}
 
+//function to create a list showing results of voting
+function makeList() {
+  var resultsList = document.getElementById('results-list');
+  var listHeader = '<h3>Results</h3>' + '<ul>';
+  for (var index = 0; index < images.length; index++) {
+    var item = images[index].imageName;
+    listHeader += '<li>' + item + ' received ' + images[index].vote + ' votes' + '</li>';
+  }
+  resultsList.innerHTML += listHeader + '</ul>';
+}
 
+//function to add the list to the page
+function addList() {
+    document.getElementById('list-button').addEventListener('click', makeList);
+}
 
-//   var image = document.createElement('img');
-//   image.setAttribute('src', './images/bag.jpg');
-//   document.getElementById('images-holder').appendChild(image);
-//   image = document.createElement('img');
-//   image.setAttribute('src', './images/banana.jpg');
-//   document.getElementById('images-holder').appendChild(image);
-//   image = document.createElement('img');
-//   image.setAttribute('src', './images/cthulhu.jpg');
-//   document.getElementById('images-holder').appendChild(image);
-
-
-// for(var imagesIndex = 0; imagesIndex < images.length; imagesIndex++) {
-//   var image = document.createElement('img');
-//   image.setAttribute('src', './images/bag.jpg');
-//   document.getElementById('images-holder').appendChild(image);
-// }
+window.addEventListener('load', buildCarousel);
+window.addEventListener('load', addListener);
+window.addEventListener('load', addList);
